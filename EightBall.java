5b2882f32c9ad6006b64d285809e28151e6f459f
venuvm <46749549+venuvm@users.noreply.github.com>
2023-04-11 15:47:42 +0530

    Update EightBall.java

--- EightBall.java
+++ EightBall.java
@@ -6,6 +6,8 @@ public class EightBall {
 	public static void main(String args[]) throws Exception {
 		char[] buffer = new char[1024];
 		String filename = args[0];
+		
+		
 		try {
 			System.out.println("sample");
 			
